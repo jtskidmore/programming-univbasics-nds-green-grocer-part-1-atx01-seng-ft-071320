8776bede39901f6f0bef1c6b09389fe3aee5e829
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-10 20:59:06 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -7,27 +7,3 @@ def find_item_by_name_in_collection(item, collection)
   nil
 end
 
-def consolidate_cart(cart)
-  
-  cart.each do |hash|
-    hash[:count] = 1
-  end
-  
-  counter_hash = Hash.new(0)
-  
-  cart.each do |v|
-    counter_hash[v] += 1
-  end
-
-  new_cart = []
-
-  counter_hash.each do |key, value|
-    new_cart << key
-    new_cart[new_cart.index(key)][:count] = value
-  end
-  
-
-  
-   new_cart
-
-end
