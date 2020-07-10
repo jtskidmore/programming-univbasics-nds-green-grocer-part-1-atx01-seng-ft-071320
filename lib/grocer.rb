c3033e2b75ffbf0769a8063b4cbbe34003f6679d
JT Skidmore <johntoddskidmore@gmail.com>
2020-07-10 21:23:11 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -10,8 +10,18 @@ end
 
 def consolidate_cart(cart)
   
+  new_cart = []
+  
   cart.each do |element|
     cart[cart.index(element)][:count] = 1
+  end
+  
+  cart.each do |element|
+    if new_cart.include? element
+      new_cart[new_cart.index(element)][:count] += 1 
+    else
+      new_cart << element
+    end
   end
   
 end
